f11c4076f2b8e4472cf5d60b454fbf4ff2904538
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2023-02-15 20:06:17 -0400

    login firebase

---
+++ firebase.js
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js'
+import {
+  getAuth,
+  signInWithEmailAndPassword
+} from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyB72E8OuZZpzBt8GTQHDzfrFY56HVNslVs',
+  authDomain: 'cris-app-5a21f.firebaseapp.com',
+  projectId: 'cris-app-5a21f',
+  storageBucket: 'cris-app-5a21f.appspot.com',
+  messagingSenderId: '29953400526',
+  appId: '1:29953400526:web:d16c29f2e0b24c21087a9f'
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const buttonLoga = document.getElementById('buttonLoga')
+
+const loginArea = document.querySelector('.login')
+const calendarArea = document.querySelector('.calendarWrap')
+
+buttonLoga.addEventListener('click', () => {
+  console.log(email.value)
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then(userCredential => {
+      console.log(userCredential.user)
+      if (true) {
+        loginArea.style.display = 'none'
+        calendarArea.style.display = 'block'
+      }
+    })
+    .catch(err => console.log(err))
+})
